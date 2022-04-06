a6c2ad0135c61aad12904c41cc8bf0b2910367f8
slavax1230 <slavax25@gmail.com>
2022-04-06 02:33:31 -0700

    1.11

--- controllers/product.js
+++ controllers/product.js
@@ -7,6 +7,7 @@ const isAuth = require('../controllers/isAuth')
 const User = require('../models/user')
 const Category = require('../models/category')
 const Product = require('../models/product');
+const product = require('../models/product');
 
 router.post('/addCategory', isAuth, async(request,response) => {
     const associateId = request.account._id; 
@@ -69,39 +70,82 @@ router.delete('/deleteCategory/:categoryId', isAuth, async(request,response) => 
     
 })
 
-router.post('/addProduct', isAuth, async(request,response) => {
-    const associateId = request.account._id; 
-    const store = await Store.findOne({associateId:associateId}) // ככה מוצאים את המשתמש לפי הטוקן
-    
-    const {categoryName,priority} = request.body;
+router.post('/addProduct/:categoryId', isAuth, async(request,response) => {
+   const associateId = request.account._id;
+   const store = await Store.findOne({associateId:associateId}) // ככה מוצאים את המשתמש לפי הטוקן
+   const categoryId = request.params.categoryId;
+   const {productName,price,discount,unitInStock,desclimer,isAgeLimitation} = request.body;
 
-    const _category = new Category({
-        _id : mongoose.Types.ObjectId(),
-        storeId : store._id,
-        categoryName: categoryName,
-        priority: priority
-    });
-    _category.save()
-    .then(created_category => {
+   const _product = new Product({
+    _id : mongoose.Types.ObjectId(),
+    storeId : store._id,
+    categoryId: categoryId,
+    productName: productName,
+    price:price,
+    discount: discount,
+    unitInStock: unitInStock,
+    desclimer: desclimer,
+    isAgeLimitation:isAgeLimitation
+    
+});
+_product.save()
+.then(saved_product => {
+    return response.status(200).json({
+        message:saved_product
+    })
+})
+.catch(err => {
+    return response.status(500).json({message:err})
+})
+})
+
+router.put('/updateProduct/:productId', isAuth, async(request,response) => {
+    const associateId = request.account._id;
+    const store = await Store.findOne({associateId:associateId}) // ככה מוצאים את המשתמש לפי הטוקן
+    const productid = request.params.productId;
+
+    const {productName,price,discount,unitInStock} = request.body;
+
+    Product.findById(productid)
+    .then(product => {
+        if (product) {
+            product.productName = productName;
+            product.price = price;
+            product.discount = discount;
+            product.unitInStock = unitInStock;
+            return product.save()
+            .then(updated_product => {
+                return response.status(200).json({
+                    status:1,
+                    message:updated_product
+            })
+            .catch(err => {
+                return response.status(200).json({message: "there is no product by this ID"})
+            })
+         })
+      }else {
+        return response.status(200).json({message: "there is no product by this ID"});
+      }
+    })
+    .catch(error => {
+        return response.status(500).json({message: error.message});
+        })
+})
+
+// double search by category and prodcut id for better security.
+
+router.delete('/deleteProduct/:categoryId/:productId', isAuth, async(request,response) => { 
+    const productId = request.params.productId;
+    const categoryId = request.params.categoryId;
+    Product.findOneAndDelete({categoryId: categoryId, _id:productId})
+    .then(deleted_product => {
         return response.status(200).json({
-            status:true,
-            message: created_category
+            message:deleted_product
         })
     })
     .catch(err => {
-    return response.status(500).json({
-        status:false,
-        message: err
-        });
-    })  
-})
-
-router.put('/updateProduct', isAuth, async(request,response) => {
-
-})
-
-router.delete('/deleteProduct', isAuth, async(request,response) => {
-
+        return response.status(200).json({message:err})
+    })
 })
 
 router.get('/getAllCategories', isAuth, async(request,response) => {
